fa218c382dbbfd0043ebf23715b92d6eec4a2f7b
arafat-sabbir <arafatshabbir8@gmail.com>
2024-09-06 03:28:38 +0600

    Added Condition On Adding Product To Cart

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,45 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import allRoutes from './app/routes/index';
+const formatDate = (date: Date) => {
+  const options: Intl.DateTimeFormatOptions = {
+    month: 'numeric',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    hour12: true,
+  };
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+};
+
+// Middleware to log requests and responses
+const requestLogger = (req: Request, res: Response, next: NextFunction) => {
+  const method = req.method;
+  const url = req.url;
+  const query = JSON.stringify(req.query, null, 2);
+  const params = JSON.stringify(req.params, null, 2);
+  const formattedDate = formatDate(new Date());
+  // Override res.send to capture the response data
+  console.log('------------------------');
+  console.log(
+    `Api :- \x1b[0m\x1b[34m${method}\x1b[0m \x1b[32m${url}\x1b[0m \x1b[36m[${formattedDate}]\x1b[0m`
+  );
+  console.log('------------------------');
+  next();
+};
 
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:5173',"http://localhost:4173"], credentials: true }));
+app.use(
+  cors({
+    origin: ['https://athletes-arsenal.vercel.app', 'http://localhost:5173'],
+    credentials: true,
+  })
+);
+
+app.use(requestLogger);
 
 // use All The Routes From Routes
 app.use('/api/v1', allRoutes);
--- src/app/modules/cart/cart.service.ts
+++ src/app/modules/cart/cart.service.ts
@@ -1,9 +1,27 @@
+import AppError from '../../errors/AppError';
+import ProductModel from '../Product/product.model';
 import { TCart } from './cart.interface';
 import CartModel from './cart.model';
 
 const addNewProductToCart = async (payload: TCart) => {
+  const product = await ProductModel.findOne({ _id: payload.product });
+  if (!product) throw new Error('Product Not Found');
+  if (payload?.quantity <= product?.stockCount) {
+    product.stockCount = product.stockCount - payload.quantity;
+    await product.save();
+  } else {
+    throw new AppError(400, 'Insufficient Stock');
+  }
+  const existingCartProduct = await CartModel.findOne({
+    user: payload.user,
+    product: payload.product,
+  });
+  if (existingCartProduct) {
+    existingCartProduct.quantity = existingCartProduct.quantity + payload.quantity;
+    await existingCartProduct.save();
+    return existingCartProduct;
+  }
   const cartProduct = await CartModel.create(payload);
-
   return cartProduct;
 };
 const getMyCartProducts = async (id: string) => {
